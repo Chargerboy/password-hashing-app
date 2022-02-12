6d325f73989453c09f5d2e911c493ef6d03ca3ca
Hrishabh Goel <hrishabh.w21@gmail.com>
2022-02-12 03:14:13 -0800

    fouandtion for front & back, and database done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const App = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmP, setConfirmP] = useState(null);
+  const [error, setError] = useState(null);
+  const submit = 'submit';
 
-function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmP) {
+      setError('Make sure your Passwords match!');
+      return;
+    }
+
+    axios.post('http://localhost:8000/signup', {
+      username,
+      password,
+    });
+
+    console.log(username);
+    console.log(password);
+    console.log(confirmP);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="usrnm"
+          placeholder="username"
+          id="usrnm"
+          required
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="pswd"
+          placeholder="password"
+          id="pswd"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="cnfrm-pass"
+          placeholder="confirm password"
+          id="cnfrm-pass"
+          required
+          onChange={(e) => {
+            setConfirmP(e.target.value);
+          }}
+        />
+        <input type={submit} />
+      </form>
+      {<h1>{error}</h1>}
+    </>
   );
-}
+};
 
 export default App;
